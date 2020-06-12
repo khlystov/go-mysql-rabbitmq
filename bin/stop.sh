6649c00bd2493dea89df598c57d9dbd9b0aedf96
Max Khlystov <max.khlystov@gmail.com>
2020-06-12 15:18:12 +0300

    restart policy

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker stop go-mysql-rabbitmq 
+docker stop go-mysql-rabbitmq && docker rm go-mysql-rabbitmq || true
--- bin/update.sh
+++ bin/update.sh
@@ -2,4 +2,4 @@
 
 docker stop go-mysql-rabbitmq && docker rm go-mysql-rabbitmq || true
 docker build -t go-mysql-rabbitmq .
-docker run -d --name go-mysql-rabbitmq -v $PWD/river.toml:/app/etc/river.toml -v $PWD/var:/app/var go-mysql-rabbitmq
+docker run -d --restart always --name go-mysql-rabbitmq -v $PWD/river.toml:/app/etc/river.toml -v $PWD/var:/app/var go-mysql-rabbitmq
